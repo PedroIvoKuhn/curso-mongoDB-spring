4fd714347d9ae48bff185762a58d477353806664
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2023-04-27 11:12:21 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/pedro/cursomongo/CursomongoApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.cursomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/cursomongo/CursomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.cursomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
